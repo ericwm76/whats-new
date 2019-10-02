63daaa7b04010193889a94aa10423fca38fca41f
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-02 11:37:57 -0600

    Cause articles to render on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
 
@@ -7,14 +11,20 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local, entertainment, health, science, technology
     }
   }
 
   render () {
     return (
       <div className="app">
-        <NewsContainer />
+        <NewsContainer 
+          local={this.state.local} 
+          entertainment={this.state.entertainment} 
+          health={this.state.health} 
+          science={this.state.science} 
+          technology={this.state.technology}
+        />
       </div>
     );
   }
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle'
 
-const NewsContainer = 
+const NewsContainer = ({local}) => {
+  return (
+    local.map(article => {
+      return (
+        <NewsArticle
+          headline={article.headline} 
+          img={article.img} 
+          description={article.description}
+          url={article.url}
+          id={article.id}
+          key={article.id}
+        />
+      )
+    })
+  )
+}
 
 export default NewsContainer;
